5b7f82e46c87d6a5e5e650f9ef49413ee45ad033
Hut234 <doitdoitdoitdoitdo@gmail.com>
2024-10-31 22:54:58 +0900

    Feat(#37): UserLevel 엔티티 정의

--- src/main/java/com/devrace/domain/level/entity/UserLevel.java
+++ src/main/java/com/devrace/domain/level/entity/UserLevel.java
@@ -1,30 +1,33 @@
 package com.devrace.domain.level.entity;
 
+import com.devrace.global.entity.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class UserLevel {
+@Entity
+@Table(indexes = @Index(name = "idx_requirement", columnList = "requirement"))
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserLevel extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private UserLevelName userLevelName;
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private int requirement;
 
     @Column(nullable = false)
-    private String levelBadgeImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1DNsfskjJtWhewcdCt8_4spNsZf7lmL3wKQ&s";
+    private String imageUrl;
 }
