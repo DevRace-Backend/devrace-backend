8c767411873f15152a46f60741d14955ba4c37c5
Hut234 <doitdoitdoitdoitdo@gmail.com>
2024-11-02 00:33:16 +0900

    Feat(#42): 방명록 엔티티 정의

---
+++ src/main/java/com/devrace/domain/guest_book/entity/GuestBook.java
@@ -0,0 +1,36 @@
+package com.devrace.domain.guest_book.entity;
+
+import com.devrace.domain.user.entity.User;
+import com.devrace.global.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GuestBook extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 300, nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "my_page_owner_id", nullable = false)
+    private User myPageOwner;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "writer_id", nullable = false)
+    private User writer;
+}
