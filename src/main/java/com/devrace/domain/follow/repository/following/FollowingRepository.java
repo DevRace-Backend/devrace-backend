8d4df2db8c64f47ec5fb27b7973e47d1cca4d149
dmlal <opkl8781@naver.com>
2024-12-23 08:59:08 +0900

    [fix]  팔로잉리스트 조회 레이지로딩 문제 해결 #81

--- src/main/java/com/devrace/domain/follow/repository/following/FollowingRepository.java
+++ src/main/java/com/devrace/domain/follow/repository/following/FollowingRepository.java
@@ -3,6 +3,7 @@ package com.devrace.domain.follow.repository.following;
 import com.devrace.domain.follow.entity.Following;
 import com.devrace.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
@@ -13,5 +14,6 @@ import java.util.Optional;
 public interface FollowingRepository extends JpaRepository<Following, Long> {
     Optional<Following> findByFollowerAndFollowing(User follower, User following);
 
+    @Query("SELECT f FROM Following f JOIN fetch f.following WHERE f.follower =:user")
     List<Following> findByFollower(User user);
 }
